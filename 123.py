4d8ee7d052586c9f0874b99a13febbd85e64fcf3
th-ning <thning2018@foxmail.com>
2023-09-12 13:47:03 -0400

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,9 +1,21 @@
-# Take input from the user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+import PyPDF2
 
-# Add the numbers
-sum = num1 + num2
+# Open the PDF file in read binary mode
+with open('1.pdf', 'rb') as file:
+    # Create a PDF reader object
+    reader = PyPDF2.PdfReader(file)
 
-# Display the result
-print(f"The sum of {num1} and {num2} is: {sum}")
+    # Get the total number of pages in the PDF
+    num_pages = len(reader.pages)
+
+    # Extract text from each page and write to a text file
+    with open('1.txt', 'w', encoding='utf-8') as output_file:
+        for page_number in range(num_pages):
+            # Get a specific page object
+            page = reader.pages[page_number]
+
+            # Extract text from the page
+            text = page.extract_text()
+
+            # Write the text to the output file
+            output_file.write(text)
